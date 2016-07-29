c47d7abc9f00d83ac702c65da78a43ed8c337a4f
James Schloss <james.schloss@oist.jp>
2016-07-29 10:42:08 +0900

    forgot a file.

---
+++ include/init.h
@@ -0,0 +1,89 @@
+///@cond LICENSE
+/*** init.h - GPUE: Split Operator based GPU solver for Nonlinear
+Schrodinger Equation, Copyright (C) 2011-2015, Lee J. O'Riordan
+<loriordan@gmail.com>, Tadhg Morgan, Neil Crowley.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+///@endcond
+//##############################################################################
+/**
+ *  @file    evolution.h
+ *  @author  James Ryan Schloss (leios)
+ *  @date    5/31/2016
+ *  @version 0.1
+ *
+ *  @brief function for evolution.
+ *
+ *  @section DESCRIPTION
+ *  These functions and variables are necessary for carrying out the GPUE
+ *	simulations. This file will be re-written in an improved form in some
+ *	future release.
+ */
+//##############################################################################
+
+#ifndef INIT_H
+#define INIT_H
+
+#include "../include/split_op.h"
+#include "../include/kernels.h"
+#include "../include/constants.h"
+#include "../include/fileIO.h"
+#include "../include/tracker.h"
+#include "../include/minions.h"
+#include "../include/parser.h"
+#include "../include/ds.h"
+#include "../include/unit_test.h"
+
+#include "../include/lattice.h"
+#include "../include/node.h"
+#include "../include/edge.h"
+#include "../include/manip.h"
+#include "../include/vort.h"
+#include "../include/evolution.h"
+#include <string>
+#include <iostream>
+/* Function declarations */
+/*
+ * arg1 = Function result code from CUDA CUFFT calls.
+ * arg2 = String data for name of function called. Prints value to stdout.
+ */
+
+// UPDATE LIST LATER
+ /**
+ * @brief	Initializes data structures
+ * @ingroup	data
+ * @param	Operator class
+ * @param	Cuda class
+ * @param	Grid class
+ * @param	Wave class
+ */
+int initialise(Op &opr, Cuda &cupar, Grid &par, Wave &wave);
+
+#endif
