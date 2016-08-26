193c48888dca909b3735ebb3cb91219c75469d57
James Schloss <jrs.schloss@gmail.com>
2016-08-26 15:48:15 +0900

    adding multiple new plotting functions to plot.py

--- py/plot.py
+++ py/plot.py
@@ -63,6 +63,73 @@ def plot_wfc(xDim, yDim, data_dir, pltval, start, end, incr):
         #fig = plt.figure()
         #fig.savefig('wfc.png')
 
+# Function to plot complex vals with pltvar as the variable
+def plot_complex(xDim, yDim, data_dir, pltval, start, end, incr):
+
+    data_real = "../" + data_dir + "/" + pltval + "_0"
+    data_im = "../" + data_dir + "/" + pltval + "i_0"
+    
+    lines_real = np.loadtxt(data_real)
+    lines_im = np.loadtxt(data_im)
+    wfc_real = np.reshape(lines_real, (xDim,yDim));
+    wfc_im = np.reshape(lines_im, (xDim,yDim));
+    
+    wfc = abs(wfc_real + 1j * wfc_im)
+    wfc = wfc * wfc
+        
+    plt.imshow(wfc, extent=(1,xDim,1,yDim), interpolation='nearest',
+               cmap = cm.jet)
+    plt.colorbar()
+    plt.show()
+    #fig = plt.figure()
+    #fig.savefig('wfc.png')
+
+# Function to plot wfc with pltvar as a variable to modify the type of plot
+def plot_wfc_k(xDim, yDim, data_dir, pltval, start, end, incr):
+    for i in range(start,end,incr):
+        data_real = "../" + data_dir + "/wfc_0_const_%s" % i
+        data_im = "../" + data_dir + "/wfc_0_consti_%s" % i
+
+        lines_real = np.loadtxt(data_real)
+        lines_im = np.loadtxt(data_im)
+        wfc_real = np.reshape(lines_real, (xDim,yDim));
+        wfc_im = np.reshape(lines_im, (xDim,yDim));
+
+        wfc = (wfc_real + 1j * wfc_im)
+
+        wfc_k = np.fft.fft2(wfc)
+        wfc_k_plot = np.abs(np.fft.fftshift(wfc_k))
+        wfc_k_plot = wfc_k_plot**2
+
+        plt.imshow(wfc_k_plot, extent=(1,xDim,1,yDim), interpolation='nearest',
+                   cmap = cm.jet)
+        plt.colorbar()
+        plt.show()
+        #fig = plt.figure()
+        #fig.savefig('wfc.png')
+
+# Function to plot wfc with pltvar as a variable to modify the type of plot
+def plot_wfc_phase(xDim, yDim, data_dir, pltval, start, end, incr):
+    for i in range(start,end,incr):
+        data_real = "../" + data_dir + "/wfc_0_const_%s" % i
+        data_im = "../" + data_dir + "/wfc_0_consti_%s" % i
+
+        lines_real = np.loadtxt(data_real)
+        lines_im = np.loadtxt(data_im)
+        wfc_real = np.reshape(lines_real, (xDim,yDim));
+        wfc_im = np.reshape(lines_im, (xDim,yDim));
+
+        wfc = (wfc_real + 1j * wfc_im)
+
+        wfc = np.angle(wfc)
+        plt.imshow(wfc, extent=(1,xDim,1,yDim), interpolation='nearest',
+                   cmap = cm.jet)
+        plt.colorbar()
+        plt.show()
+        #fig = plt.figure()
+        #fig.savefig('wfc.png')
+
+
 # Function to parse arguments for plotting
 # Note: We assume that the parameters come in sets
 def parse_args(string_list):
@@ -95,6 +162,15 @@ def plot(par):
     if (par.item == "wfc"):
         plot_wfc(par.xDim, par.yDim, par.data_dir, par.item, 
                  par.start, par.end, par.incr)
+    elif (par.item == "wfc_k"):
+        plot_wfc_k(par.xDim, par.yDim, par.data_dir, par.item,
+                   par.start, par.end, par.incr)
+    elif (par.item == "wfc_phase"):
+        plot_wfc_phase(par.xDim, par.yDim, par.data_dir, par.item,
+                       par.start, par.end, par.incr)
+    elif (par.item == "GK" or par.item == "GV"):
+        plot_complex(par.xDim, par.yDim, par.data_dir, par.item,
+                     par.start, par.end, par.incr)
     else:
         plot_var(par.xDim, par.yDim, par.data_dir, par.item)
 
