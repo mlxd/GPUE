631bcdb5e4d92571759471e02a33c2d1123b67f6
James Schloss <jrs.schloss@gmail.com>
2016-11-22 17:52:28 +0900

    adding small function to create bvoxel file for blender rendering.

---
+++ py/gen_data.py
@@ -0,0 +1,83 @@
+#-------------gen_data.py------------------------------------------------------##
+# Purpose: This file will take the data from GPUE and turn it into a bvox file
+#          for visualization with blender
+#
+#------------------------------------------------------------------------------#
+
+import numpy as np
+
+xDim = yDim = zDim = 128
+
+# Function to plot wfc with pltvar as a variable to modify the type of plot
+def wfc_density(xDim, yDim, zDim, data_dir, pltval, i):
+    print(i)
+    data_real = "../" + data_dir + "/wfc_0_const_%s" % i
+    data_im = "../" + data_dir + "/wfc_0_consti_%s" % i
+    lines_real = np.loadtxt(data_real)
+    lines_im = np.loadtxt(data_im)
+    print(len(lines_real))
+    wfc_real = np.reshape(lines_real, (xDim,yDim,zDim));
+    wfc_im = np.reshape(lines_im, (xDim,yDim, zDim));
+    wfc = abs(wfc_real + 1j * wfc_im)
+    wfc = wfc * wfc
+    #print(wfc)
+    return wfc
+
+def wfc_phase(xDim, yDim, zDim, data_dir, pltval, i):
+    print(i)
+    data_real = "../" + data_dir + "/wfc_0_const_%s" % i
+    data_im = "../" + data_dir + "/wfc_0_consti_%s" % i
+    lines_real = np.loadtxt(data_real)
+    lines_im = np.loadtxt(data_im)
+    wfc_real = np.reshape(lines_real, (xDim,yDim, zDim));
+    wfc_im = np.reshape(lines_im, (xDim,yDim, zDim));
+    wfc = (wfc_real + 1j * wfc_im)
+    wfc = np.angle(wfc)
+    wfc = np.reshape(wfc,(xDim*yDim*zDim))
+    maximum = max(wfc)
+    minimum = min(wfc)
+    for i in range(0,len(wfc)):
+        wfc[i] = (wfc[i] - minimum) / (maximum - minimum)
+    wfc = np.reshape(wfc,(xDim,yDim,zDim))
+    return wfc
+
+def var(xDim, yDim, zDim, data_dir, pltval):
+    data = "../" + data_dir + "/" + pltval
+    lines = np.loadtxt(data)
+    maximum = max(lines)
+    minimum = min(lines)
+    for i in range(0,len(lines)):
+        lines[i] = (lines[i] - minimum) / (maximum - minimum)
+    val = np.reshape(lines, (xDim,yDim,zDim));
+    return val
+
+def proj_2d(xDim, yDim, zDim, data_dir, pltval, i):
+    filename = "../" + data_dir + "/wfc_0"
+    print(i)
+    data_real = "../" + data_dir + "/wfc_0_const_%s" % i
+    data_im = "../" + data_dir + "/wfc_0_consti_%s" % i
+    lines_real = np.loadtxt(data_real)
+    lines_im = np.loadtxt(data_im)
+    print(len(lines_real))
+    wfc_real = np.reshape(lines_real, (xDim,yDim,zDim));
+    wfc_im = np.reshape(lines_im, (xDim,yDim, zDim));
+    wfc = abs(wfc_real + 1j * wfc_im)
+    wfc = wfc * wfc
+    file = open(filename,'w')
+    for k in range(0,xDim):
+        for j in range(0,yDim):
+            file.write(str(wfc[zDim/2][k][j]) + '\n')
+    file.close()
+
+#proj_2d(xDim, yDim, zDim,"data","wfc",45000)
+
+#item = wfc_density(xDim, yDim, zDim,"data","wfc",2)
+#item = wfc_phase(xDim, yDim, zDim,"data","wfc",45000)
+item = var(xDim, yDim, zDim,"data","V_0")
+
+nx, ny, nz, nframes = xDim, yDim, zDim,1
+header = np.array([nx,ny,nz,nframes])
+
+binfile = open('test1.bvox','wb')
+header.astype('<i4').tofile(binfile)
+item.astype('<f4').tofile(binfile)
