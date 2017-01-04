e258de4a50bf9ff6477491fb7246bfb5064d53c1
James Schloss <james.schloss@oist.jp>
2017-01-04 15:22:56 +0900

    removing license from each individual file, and instead putting it into a standard LICENSE.txt file.

--- src/init.cu
+++ src/init.cu
@@ -1,35 +1,3 @@
-/*
-* init.cu - GPUE: Split Operator based GPU solver for Nonlinear 
-Schrodinger Equation, Copyright (C) 2011-2015, Lee J. O'Riordan 
-<loriordan@gmail.com>, Tadhg Morgan, Neil Crowley. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without 
-modification, are permitted provided that the following conditions are 
-met:
-
-1. Redistributions of source code must retain the above copyright 
-notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright 
-notice, this list of conditions and the following disclaimer in the 
-documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its 
-contributors may be used to endorse or promote products derived from 
-this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 
 #include "../include/init.h"
 
@@ -694,9 +662,9 @@ int init_3d(Op &opr, Cuda &cupar, Grid &par, Wave &wave){
                                                ( 1 - omega*omega) ) ));
 
     //std::cout << "Rxy is: " << Rxy << '\n';
-    double xMax = 100*bec_length;//6*Rxy*a0x; //6*Rxy*a0x;
-    double yMax = 100*bec_length;//6*Rxy*a0y; 
-    double zMax = 100*bec_length;//6*Rxy*a0z;
+    double xMax = 10*bec_length;//6*Rxy*a0x; //6*Rxy*a0x;
+    double yMax = 10*bec_length;//6*Rxy*a0y; 
+    double zMax = 10*bec_length;//6*Rxy*a0z;
     par.store("xMax",xMax);
     par.store("yMax",yMax);
     par.store("zMax",zMax);
@@ -1003,8 +971,10 @@ int init_3d(Op &opr, Cuda &cupar, Grid &par, Wave &wave){
     par.store("omegaZ", omegaZ);
     par.store("dx", dx);
     par.store("dy", dy);
+    par.store("dz", dz);
     par.store("xMax", xMax);
     par.store("yMax", yMax);
+    par.store("zMax", zMax);
     par.store("winding", l);
     par.store("x", x);
     par.store("y", y);
